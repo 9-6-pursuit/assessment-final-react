24c8c27da7f74f3d466997446e0e699b429c6d19
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-06-11 10:53:13 -0400

    finished movie.js components

--- src/App.js
+++ src/App.js
@@ -3,15 +3,10 @@ import Nav from "./components/Nav";
 import Home from "./components/Home";
 import Movies from "./components/Movies";
 
-import { useState } from "react";
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-    const[selected, setSelected] = useState(null)
 
-
-  console.log(selected)
 
   return (
     <div className="App">
@@ -19,6 +14,7 @@ function App() {
         <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />}/>
 
         </Routes>
       </Router>
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,53 @@
 import "./Movies.css"
+import { useState, useEffect } from "react";
+
 function Movies() {
+
+    const [movies, setMovies] = useState([])
+    const [selected, setSelected] = useState (null)
+
+    useEffect(()=> {
+        fetch('https://resource-ghibli-api.onrender.com/films')
+        .then((response) => response.json())
+        .then(data => {
+            console.log(data)
+            setMovies(data)
+        })
+        .catch(error =>{
+            console.error(error)
+        })
+    }, [])
+
+
+    let handleOnChange = (event) => {
+        console.log(event.target.value)
+        let title = event.target.value
+        let selecteedMovie = movies.find(movie => {
+            return movie.title === title
+        })
+        setSelected(selecteedMovie)
+    }
+
+
     return (
       <div className="movie">
-          <h3>Select a Movie</h3>
+            <h1>Select a Movie</h1>
+        <select onChange={handleOnChange}>
+        <option value=""></option>
+            {movies.map((movie) => {
+                return(
+                <option key={movie.id} >{movie.title}</option>
+                )
+            })}
+        </select>
+        {selected ? (
+            <div>
+                <h3>Title: {selected.title}</h3>
+                <p><strong>Release Date:</strong> {selected.release_date}</p>
+                <p><strong>Description:</strong> {selected.description}</p>
+            </div>
+        ): null}
+
       </div>
     );
   }
