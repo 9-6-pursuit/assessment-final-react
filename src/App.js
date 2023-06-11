5d4a62cf564fc0b6c3f57f5a03b66f079a3664b2
Jerry John <jerryjohn@jerrys-air.myfiosgateway.com>
2023-06-11 13:16:32 -0400

    Push 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import Movies from "./components/Movies";
+import Character from "./components/Character";
+import Locations from "./components/Locations";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="app">
-      <h1>Welcome to GhibliApp</h1>
-    </div>
+    <Router>
+      {/* <Nav> */}
+      <Routes>
+        <Route path="/movies" element= {<Movies/>}/>
+        <Route path="/character" element= {<Character/>}/>
+        <Route path="/location" element= {<Locations/>}/>
+      </Routes>
+    </Router>
   );
 }
 
