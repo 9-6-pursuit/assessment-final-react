93fc438671d85e4604e8a1e4b6aafbbe1fc9eb00
antunishdPursuit <dennysantunish@pursuit.org>
2023-06-11 12:23:36 -0400

    completed movies, navbar,people componen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./Components/NavBar"
+import Home from "./Components/Home"
+import Movies from "./Components/Movies";
+import People from "./Components/People";
+import Locations from "./Components/Locations";
+
 function App() {
   return (
     <div className="app">
-      <h1>Welcome to GhibliApp</h1>
+      <Router>
+        <NavBar/>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/movies" element={<Movies/>}/>
+            <Route path="/people" element={<People/>}/>
+            <Route path="/locations" element={<Locations/>}/>
+          </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Locations.js
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import "./locations.css"
+function Locations() {
+    const [locations, setLocations] = useState([])
+    const [shown, setShown] = useState(false)
+
+    useEffect(() => {
+        fetch(`https://resource-ghibli-api.onrender.com/locations`)
+        .then(response => response.json())
+        .then(locations => {
+            setLocations(locations)
+        })
+        .catch(message => {
+            console.error(message)
+        })
+    }, [])
+
+    function handleLocations() {
+        if(shown === false ){
+            setShown(true)
+        } else {
+            setShown(false)
+        }
+    }
+    function sortClimate(){
+        setLocations(locations.sort((a, b) => {
+            let fa = a.climate.toLowerCase(),
+            fb = b.climate.toLowerCase();
+    
+        if (fa < fb) {
+            return -1;
+        }
+        if (fa > fb) {
+            return 1;
+        }
+        return 0;
+        }))
+        console.log(locations)
+    }
+    function sortTerrain(){
+        setLocations(locations.sort((a, b) => {
+            let fa = a.terrain.toLowerCase(),
+            fb = b.terrain.toLowerCase();
+    
+        if (fa < fb) {
+            return -1;
+        }
+        if (fa > fb) {
+            return 1;
+        }
+        return 0;
+        }))
+        console.log(locations)
+    }
+    return (
+        <div className="locations text-center">
+            <h1>List of Locations</h1>
+            <button onClick={handleLocations} type="submit" className="btn btn-primary btn-lg search">{shown ? "Show Locations" : "Hide Locations"}</button>
+            {shown ? <button onClick={sortClimate} type="submit" className="btn btn-primary btn-lg search"> Sort by Climate</button> : null}
+            {shown ? <button onClick={sortTerrain} type="submit" className="btn btn-primary btn-lg search"> Sort by Terrain</button> : null}
+            <div className="container">
+                <div className="row justify-content-md-center">
+                    {shown ? locations.map(location => {
+                        return (
+                            <div key={location.id}className="col-3 card"> 
+                                <p className="lineItem"><strong>Name: </strong>{location.name}</p>
+                                <p className="lineItem"><strong>Climate: </strong>{location.climate}</p>
+                                <p className="lineItem"><strong>Terrain: </strong>{location.terrain}</p>
+                            </div>
+                        )
+                    }) : null}
+                </div>
+            </div>
+
+        </div>
+  )
+  }
+  
+  export default Locations;
+  
