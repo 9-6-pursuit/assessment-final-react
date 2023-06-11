9c1929919876690c21725ea18e636c9de04d0e25
winterbelle <blancaaltamirano@pursuit.org>
2023-06-11 11:12:55 -0400

    Home and navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import NavBar from "./NavBar";
+
 function App() {
   return (
     <div className="app">
-      <h1>Welcome to GhibliApp</h1>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
