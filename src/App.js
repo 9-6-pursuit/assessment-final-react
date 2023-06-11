f58d0ba0f47e1a2dc451419bb01609a16bb6e0ec
Berlin Rivas <berlinrivas@Berlins-MacBook-Air.local>
2023-06-11 11:20:27 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import './App.css'
+import Nav from './Components/Nav'
+import Home from './Components/Home'
+import People from './Components/People'
+import Locations from './Components/Locations'
+import Movies from './Components/Movies'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './index.css'
+
+
 function App() {
   return (
     <div className="app">
-      <h1>Welcome to GhibliApp</h1>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />}/>
+          <Route path="/people" element={<People />}/>
+          <Route path="/locations" element={<Locations />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Locations.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+
+export default function Locations() {
+  const [showLocations, setShowLocations] = useState(false);
+  const [locations, setLocations] = useState([]);
+  const [sortBy, setSortBy] = useState("");
+
+  useEffect(() => {
+    fetch("https://resource-ghibli-api.onrender.com/locations")
+      .then((response) => response.json())
+      .then((data) => {
+        setLocations(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const toggleLocations = () => {
+    setShowLocations(!showLocations);
+  };
+
+  const sortByName = () => {
+    setSortBy("name");
+  };
+
+  const sortByClimate = () => {
+    setSortBy("climate");
+  };
+
+  const sortByTerrain = () => {
+    setSortBy("terrain");
+  };
+
+  const sortLocations = (a, b) => {
+    if (sortBy === "name") {
+      return a.name.localeCompare(b.name);
+    } else if (sortBy === "climate") {
+      return a.climate.localeCompare(b.climate);
+    } else if (sortBy === "terrain") {
+      return a.terrain.localeCompare(b.terrain);
+    }
+    return 0;
+  };
+
+  return (
+    <div className="locations">
+      <h1>List of Locations</h1>
+      <button onClick={toggleLocations}>
+        {showLocations ? "Hide Locations" : "Show Locations"}
+      </button>
+      <button onClick={sortByName}>Sort by Name</button>
+      <button onClick={sortByClimate}>Sort by Climate</button>
+      <button onClick={sortByTerrain}>Sort by Terrain</button>
+      {showLocations && (
+        <ul className="location-list">
+          {locations.sort(sortLocations).map((location) => (
+            <li key={location.id}>
+              <h3>Name: {location.name}</h3>
+              <ul>
+                <li className="list">Climate: {location.climate}</li>
+                <li className="list">Terrain: {location.terrain}</li>
+              </ul>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
