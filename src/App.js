02819f4b9903f3d42e65a609de598afa65cbebd7
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-06-11 10:23:48 -0400

    first function done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+
+import Home from "./components/Home"
+import People from "./components/People"
+import Locations from "./components/Locations"
+import Nav from "./components/Nav"
+import Movies from "./components/Movies"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+
+
+
 function App() {
   return (
     <div className="app">
-      <h1>Welcome to GhibliApp</h1>
+            <Router>
+        <Nav/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/movies" element={<Movies/>}/>
+          <Route path="/people" element={<People/>} />
+          <Route path="/locations" element={<Locations/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import homeIcon from '/Users/joanavelpascual/Pursuit Documents/Module_3/mod-3-final/assessment-final-react/src/components/Images/home-icon.png';
+import './Nav.css';
+
+export default function Nav() {
+  return (
+    <div className="nav-container">
+      <Link className='home-link' to="/">
+        <img className="home-icon" src={homeIcon} alt="Home Icon" />
+      </Link>
+      <Link className='nav-link movies-link' to="/movies">Movies</Link>
+      <Link className='nav-link people-link' to="/people">People</Link>
+      <Link className='nav-link locations-link' to="/locations">Locations</Link>
+    </div>
+  );
+}
