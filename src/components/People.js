6cf056c89b5f6aee8e02618de027a7b9bec934e5
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2023-06-11 11:47:54 -0400

    People is working...ish

--- src/components/People.js
+++ src/components/People.js
@@ -4,7 +4,9 @@ import { useState, useEffect } from 'react'
 function People() {
 
     const [peoples, setPeoples] = useState([])
+    const [names, setNames] = useState("")
     const [selectedPeoples, setSelectedPeoples] = useState(null)
+
     
     useEffect(()=>{
         fetch(`https://resource-ghibli-api.onrender.com/people`)
@@ -18,15 +20,38 @@ function People() {
         })
     },[])
 
+    let  handleTextChange = (event) =>{
+        setNames(event.target.value)
+    }
 
-
-
-
-
-
+    let handleClick = (event) =>{
+        event.preventDefault()
+        const result = peoples.filter(people => people.name === names)
+        if (result.length > 0) {
+            setSelectedPeoples(result[0])
+          } else {
+            setSelectedPeoples("Not Found")
+          }
+      
+    }
   return (
     <div>
         <h2>Search for a Person</h2>
+        <form>
+        <input type='text' id='search' onChange={handleTextChange} />
+        <button type='submit' onClick={handleClick}>
+          submit
+        </button>
+        {selectedPeoples ? (
+          <div>
+            <h3>Name: {selectedPeoples.name}</h3>
+            <p><strong>Age:</strong> {selectedPeoples.age}</p>
+            <p><strong>Gender:</strong> {selectedPeoples.gender}</p>
+            <p><strong>Hair color:</strong> {selectedPeoples.hair_color}</p>
+          </div>
+        ): null}
+        
+      </form>
     </div>
   )
 }
