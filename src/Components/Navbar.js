916f635b68bec372ad4717730d88583dc3f79684
Adrian Burke <114108495+AdrianBurke1@users.noreply.github.com>
2023-06-11 09:54:57 -0400

    assessment-final-react9.6

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,26 +7,29 @@ import './Navbar.css'; // Import the CSS file
 
 
 const Navbar = () => {
-  return (
-    <nav>
-      <div className="logo">
-        <Link to="/">
-          <img src="logo.png" alt="Logo" width="50" height="50" />
-        </Link>
-      </div>
-      <ul className="nav-links">
-        <li>
-          <Link to="/movies">Movies</Link>
-        </li>
-        <li>
-          <Link to="/people">People</Link>
-        </li>
-        <li>
-          <Link to="/locations">Locations</Link>
-        </li>
-      </ul>
-    </nav>
-  );
-};
-
-export default Navbar;
+    return (
+      <nav>
+        <div className="logo">
+          <Link to="/">
+            <img src="https://w7.pngwing.com/pngs/401/779/png-transparent-studio-ghibli-hd-logo.png" alt="Logo" width="50" height="50" />
+          </Link>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/movies">Movies</Link>
+          </li>
+          <li>
+            <Link to="/people">People</Link>
+          </li>
+          <li>
+            <Link to="/locations">Locations</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  };
+  
+  export default Navbar;
